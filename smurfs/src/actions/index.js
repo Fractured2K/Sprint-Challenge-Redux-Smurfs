b60990ca4396910af7cd067e23f137f156505e59
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-08 08:20:37 -0800

    Scaffolded SmurfList Component

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -4,18 +4,21 @@ export const GET_SMURF_START = "GET_SMURF_START";
 export const GET_SMURF_SUCCESS = "GET_SMURF_SUCCESS";
 export const GET_SMURF_FAILURE = "GET_SMURF_FAILURE";
 
-export const getSmurf = () => dispatch => {
+export const getSmurfs = () => dispatch => {
 	dispatch({ type: GET_SMURF_START });
 
-	axios.get("http://localhost:3333/smurfs").then(res => {
-		dispatch({
-			type: GET_SMURF_SUCCESS,
-			payload: res.data
-		}).catch(err => {
+	axios
+		.get("http://localhost:3333/smurfs")
+		.then(res => {
+			dispatch({
+				type: GET_SMURF_SUCCESS,
+				payload: res.data
+			});
+		})
+		.catch(err => {
 			dispatch({
 				type: GET_SMURF_FAILURE,
 				payload: err
 			});
 		});
-	});
 };
--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getSmurfs } from "../actions";
 
 class SmurfList extends Component {
+	componentDidMount() {
+		this.props.getSmurfs();
+	}
+
 	render() {
-		return <h1>Hello </h1>;
+		return (
+			<div>
+				<h1>Hello</h1>
+				{console.log(this.props)}
+			</div>
+		);
 	}
 }
 
-export default SmurfList;
+const mapStateToProps = (state, ownProps) => ({
+	smurfs: state.smurfs
+});
+
+export default connect(
+	mapStateToProps,
+	{ getSmurfs }
+)(SmurfList);
