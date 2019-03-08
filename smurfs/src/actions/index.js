a7b5c4fb8272739e7e3d5393acfb6f8283374b89
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-08 08:07:47 -0800

    Created smurf action types

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,3 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
-
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+export const GET_SMURF_START = "GET_SMURF_START";
+export const GET_SMURF_SUCCESS = "GET_SMURF_SUCCESS";
+export const GET_SMURF_FAILURE = "GET_SMURF_FAILURE";
